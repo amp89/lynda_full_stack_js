87c850d22d14286004e6430e59e110c79e78706c
Alex Peterson <alexander.peterson89@gmail.com>
2017-07-18 22:32:09 -0400

    basic node servers

--- code/server.js
+++ code/server.js
@@ -1,5 +1,41 @@
-import config, { nodeEnv, logStars } from "./config";
+/*
+import http from 'http'
 
-logStars("i imported this function!!!!");
+http.get("http://www.alexmpeterson.com", res => {
+    console.log(res.statusCode);
+    res.on('data',chunk => {
+        console.log(chunk.toString());
+    });
 
-logStars(nodeEnv);
+} );
+*/
+
+import http from 'http';
+
+
+/*
+const server = http.createServer();
+
+server.listen(8080);
+
+server.on('request',(req,res) => {
+        res.write('hello!!!\n\n');
+        setTimeout(()=>{
+            res.write('\n\ngooodbye!\n\n')
+            res.end();
+        },3000)
+
+});
+*/
+
+const server = http.createServer((req,res) => {
+
+    res.write("hi\n\n\n");
+    setTimeout(()=>{
+        res.write("byeeeee\n\n\n");
+        res.end();
+    },2000);
+
+});
+
+server.listen(8080);
