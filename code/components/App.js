ecacbb6902a692f72ed886b3fd7436d49a02ee0d
Alex Peterson <alexander.peterson89@gmail.com>
2017-07-21 02:43:15 -0400

    state

--- code/components/App.js
+++ code/components/App.js
@@ -2,7 +2,7 @@ import React from "react"
 
 import Header from './Header'
 
-
+/*
 const App = (props) => {
 
     return (
@@ -19,6 +19,27 @@ const App = (props) => {
 App.proptypes = {
     headerMessage: React.PropTypes.string.isRequired
 };
+*/
+
+
+class App extends React.Component {
+
+    state = {
+        pageHeader: "uhhhh headdddduuuuuuuuurrrrrrr"
+    };
+
+    render() {
+        return (
+            <div className="App">
+                <Header message={this.state.pageHeader} />
+                <div>{this.state.test}</div>
+            </div>
+        )
+    };
+
+};
+
+
 
 
 export default App;
