b0e7b0a7c5488f3669313c4b92d79c189a865bcd
Alex Peterson <alexander.peterson89@gmail.com>
2017-07-24 23:37:08 -0400

    read/modify state

--- code/src/components/App.js
+++ code/src/components/App.js
@@ -1,7 +1,10 @@
-import React from "react"
+import React from "react";
 
-import Header from './Header'
-import ContestPreview from './ContestPreview'
+import Header from './Header';
+import ContestPreview from './ContestPreview';
+
+import data from '../testData.json';
+
 /*
 const App = (props) => {
 
@@ -25,12 +28,16 @@ App.proptypes = {
 class App extends React.Component {
 
     state = {
-        pageHeader: "uhhhh headdddduuuuuuuuurrrrrrr"
+        pageHeader: "uhhhh headdddduuuuuuuuurrrrrrr",
+        contests: []
     };
 
     componentDidMount(){
         console.log("App did mount");
         //debugger;
+        this.setState({
+            contests: data.contests
+        });
     };
 
     componentWillUnmount(){
@@ -46,7 +53,7 @@ class App extends React.Component {
                 <Header message={this.state.pageHeader} />
                 <div>
                     {
-                        this.props.contests.map(contest => <ContestPreview {...contest}/>)
+                        this.state.contests.map(contest => <ContestPreview key={contest.id} {...contest}/>)
                     }
                 </div>
                 <div>{this.state.test}</div>
